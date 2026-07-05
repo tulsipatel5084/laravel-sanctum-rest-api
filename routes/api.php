def5f2968d34f02d2ed89977a032458939e3fe31
Tulsi Patel <tulsipatel5084@gmail.com>
2026-07-05 12:10:00 +0530

    Fatch user details after login

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
+use Illuminate\Http\Request;
 
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
+Route::middleware('auth:sanctum')->group(function (){
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+});
